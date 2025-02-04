827eac3ad0fb00eb14f642fc02b498afa59fc070
G S Kishan <95840962+gskishan@users.noreply.github.com>
2025-02-04 20:53:13 +0530

    Update employee.py

--- account_mapper/account_mapper/doctype/employee/employee.py
+++ account_mapper/account_mapper/doctype/employee/employee.py
@@ -1,22 +1,22 @@
 # # Copyright (c) 2025, Kiranmai and contributors
 # # For license information, please see license.txt
 
-# import frappe
-# from frappe.model.document import Document
+import frappe
+from frappe.model.document import Document
 
 
-# class Employee(Document):
+class Employee(Document):
 
-# 	def before_insert(self):
-# 		# Create a new User only if e_mail_id and employee_full_name are set
-# 		if self.e_mail_id and self.employee_full_name:
-# 			user = frappe.new_doc("User")
-# 			user.email = self.e_mail_id
-# 			user.first_name = self.employee_full_name
-# 			user.enabled = 1  # Enable user
-# 			user.add_roles("Employee")
-# 			user.insert(ignore_permissions=True)  # Insert user ignoring permissions
+	def before_insert(self):
+		# Create a new User only if e_mail_id and employee_full_name are set
+		if self.e_mail_id and self.employee_full_name:
+			user = frappe.new_doc("User")
+			user.email = self.e_mail_id
+			user.first_name = self.employee_full_name
+			user.enabled = 1  # Enable user
+			user.add_roles("Employee")
+			user.insert(ignore_permissions=True)  # Insert user ignoring permissions
 			
-# 			# Assign the Employee role
-# 			# frappe.get_doc("User", user.name).add_roles("Employee")
+			# Assign the Employee role
+			# frappe.get_doc("User", user.name).add_roles("Employee")
 
